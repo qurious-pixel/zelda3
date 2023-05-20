1f8d8f9332eb91956490b4b66887c4bc2ffa7a74
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-05-19 19:53:20 -0700

    Update and rename Info.plist to setup.py

---
+++ platform/macosx/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+APP = ['restool.py']
+DATA_FILES = []
+OPTIONS = {
+    'packages': ['PIL','yaml'],
+    'iconfile': 'zelda3.icns',
+    'plist': {
+        'CFBundleDevelopmentRegion': 'English',
+        'CFBundleIdentifier': "io.github.snesrev.Zelda3",
+        'CFBundleVersion': "1.0.0",
+        'CFBundleExecutable': "zelda3.sh",
+        'NSHumanReadableCopyright': u"Copyright © 2023, snesrev, All Rights Reserved"
+    }
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
