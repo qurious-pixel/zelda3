dd63ae3e6de8742885094eb292af2f453ffc1a3d
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-06-06 21:14:40 -0700

    Update appimage.sh

--- platform/appimage/appimage.sh
+++ platform/appimage/appimage.sh
@@ -7,13 +7,13 @@ curl -sSfLO https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-con
 chmod +x linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-plugin-conda.sh
 $LINUXDEPLOY --appdir=AppDir
-cp zelda3 AppDir/usr/bin/
+cp {zelda3,sm,smw} AppDir/usr/bin/
 cp platform/appimage/{AppRun,zelda3.desktop,zelda3.png} AppDir
 chmod +x AppDir/AppRun
 chmod +x AppDir/usr/bin/zelda3
 mkdir -p AppDir/usr/assets
-cp -r {tables,other} AppDir/usr/assets/
-cp zelda3.ini AppDir/usr/assets/
+cp -r {tables,other,smb} AppDir/usr/assets/
+cp {sm.ini,smw.ini,zelda3.ini} AppDir/usr/assets/
 cp /usr/bin/notify-send AppDir/usr/bin/
 $LINUXDEPLOY --appdir=AppDir --plugin conda --output appimage
 mkdir uploads
